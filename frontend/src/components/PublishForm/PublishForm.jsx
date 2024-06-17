8e10526ad89ce0ec445a287bbbb0013cf71ae4ae
morzdz <manon.drozdz@gmail.com>
2024-06-17 12:09:20 +0200

    feat: reset du formulaire et création de l'objet 'post' au submit

--- frontend/src/components/PublishForm/PublishForm.jsx
+++ frontend/src/components/PublishForm/PublishForm.jsx
@@ -1,6 +1,6 @@
+import { useRef } from "react";
 import { Formik, Form, useField } from 'formik';
 import * as Yup from 'yup';
-// import "../../App.css";
 
 // Composant réutilisable pour les champs de texte
 const MyTextInput = ({ label, ...props }) => {
@@ -9,7 +9,7 @@ const MyTextInput = ({ label, ...props }) => {
     return (
         <div className="flex flex-col p-2">
             <label htmlFor={props.id || props.name}>{label}</label>
-            <input {...field} {...props} />
+            <input className='border-solid border-2' {...field} {...props} />
             {meta.touched && meta.error ? (
                 <div className="error">{meta.error}</div>
             ) : null}
@@ -24,7 +24,7 @@ const MySelect = ({ label, ...props }) => {
     return (
         <div className="flex flex-col p-2">
             <label htmlFor={props.id || props.name}>{label}</label>
-            <select {...field} {...props} />
+            <select className='border-solid border-2' {...field} {...props} />
             {meta.touched && meta.error ? (
                 <div className="error">{meta.error}</div>
             ) : null}
@@ -39,17 +39,30 @@ const MyFileInput = ({ label, setFieldValue, ...props }) => {
     const handleChange = (event) => {
         const file = event.currentTarget.files[0];
         setFieldValue(props.name, file);
+        console.log(props.name, file);
     };
 
+    // Utilisation de React useRef pour créer une référence mutable qui persiste pendant tout le cycle de vie du composant.
+    const inputFile = useRef(null);
+
+    // Déclaration de la fonction handleRemove qui réinitialise l'input file.
     const handleRemove = () => {
-        setFieldValue(props.name, null);
+        // Vérifie si la référence inputFile pointe vers un élément DOM.
+        if (inputFile.current) {
+            // Réinitialise la valeur de l'input file à une chaîne vide, effaçant ainsi le fichier sélectionné.
+            inputFile.current.value = "";
+            
+            // Change le type de l'input de "file" à "text" temporairement, puis le remet à "file". Cela force un re-render de l'input, permettant ainsi de sélectionner le même fichier à nouveau.
+            inputFile.current.type = "text";
+            inputFile.current.type = "file";
+        }
     };
 
     return (
         <div className="flex flex-col items-start p-2">
             <label htmlFor={props.id || props.name}>{label}</label>
-            <input {...props} onChange={handleChange} />
-            <button type="button" onClick={handleRemove}>Retirer le fichier</button>
+            <input {...props} onChange={handleChange} ref={inputFile} />
+            <button className='border-solid border-2 p-2' type="button" onClick={handleRemove}>Retirer le fichier</button>
             {meta.touched && meta.error ? (
                 <div className="error">{meta.error}</div>
             ) : null}
@@ -58,6 +71,19 @@ const MyFileInput = ({ label, setFieldValue, ...props }) => {
 };
 
 const PublishForm = () => {
+    // Référence pour le contenu du formulaire
+    const postContent = useRef(null);
+
+    // Fonction handleReset pour réinitialiser le formulaire
+    const handleReset = (resetForm) => {
+        // Si la référence postContent est définie, réinitialise sa valeur à une chaîne vide
+        if (postContent.current) {
+            postContent.current.value = '';
+        }
+        // Réinitialise tout le formulaire
+        resetForm();
+    };
+
     return (
         <div>
             <Formik
@@ -65,7 +91,9 @@ const PublishForm = () => {
                     title: '',
                     desc: '',
                     category: '',
-                    image: null,
+                    image: '',
+                    user_id: '',
+                    message_id: ''
                 }}
                 validationSchema={Yup.object({
                     title: Yup.string()
@@ -81,7 +109,6 @@ const PublishForm = () => {
                         .test('fileFormat', 'Seuls les fichiers image sont autorisés', (value) => {
                             if (value) {
                                 if (typeof value.type === 'string') {
-                                    console.log('Type MIME du fichier:', value.type);
                                     const supportedFormats = ['jpg', 'jpeg', 'png', 'gif'];
                                     return supportedFormats.includes(value.type.split('/')[1].toLowerCase());
                                 }
@@ -96,14 +123,23 @@ const PublishForm = () => {
                             return true; // La validation réussit si aucun fichier n'est sélectionné
                         }),
                 })}
-                onSubmit={(values, { setSubmitting }) => {
-                    setTimeout(() => {
-                        alert(JSON.stringify(values, null, 2));
-                        setSubmitting(false);
-                    }, 400);
+                onSubmit={(values, { setSubmitting, resetForm }) => {
+                    const post = {
+                        id: null, // Laisser null car l'id est auto-incrémenté
+                        title: values.title,
+                        description: values.desc,
+                        illustration: values.image ? values.image.name : '',
+                        date: new Date().toISOString(), // Date actuelle
+                        user_id: null,
+                        message_id: null
+                    };
+
+                    console.log("Nouveau post :", post);
+                    setSubmitting(false);
+                    resetForm();
                 }}
             >
-                {({ setFieldValue }) => (
+                {({ setFieldValue, resetForm }) => (
                     <Form className="flex flex-col items-center p-1">
                         <MyTextInput
                             label="Titre"
@@ -134,7 +170,8 @@ const PublishForm = () => {
                             setFieldValue={setFieldValue}
                         />
 
-                        <button type="submit">Publier</button>
+                        <button className='border-solid border-2 p-2' type="button" onClick={() => handleReset(resetForm)}>Supprimer</button>
+                        <button className='border-solid border-2 p-2' type="submit">Publier</button>
                     </Form>
                 )}
             </Formik>
@@ -142,4 +179,4 @@ const PublishForm = () => {
     );
 };
 
-export default PublishForm
+export default PublishForm;
