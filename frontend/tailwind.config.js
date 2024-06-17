d2dd7a66cf1cd2166afb8ffb0ebac2bd228b8d21
Guillaume Martinez <guillaume.martinez.1804@gmail.com>
2024-06-17 17:11:12 +0200

    Début du dev de la page Home. Modification de fichiers : frontend/index.html; frontend/src/App.jsx; frontend/src/pages/Home.jsx; frontend/tailwind.config.js; package-lock.json; package.json; frontend/src/components/Carousel/; frontend/src/pages/Home.css; frontend/src/components/Card/. ATTENTION bien regarder le fichier : frontend/tailwind.config.js. + Intégration du dossier Card avec ses fichiers

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,13 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = withMT({
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./index.html", 
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+});
