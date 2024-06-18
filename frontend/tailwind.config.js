f43e9a376952239f8a06dff912f22f9a1a91537a
morzdz <manon.drozdz@gmail.com>
2024-06-18 10:54:31 +0200

    refactor: ajout de la palette de couleur dans la config tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,17 @@ module.exports = withMT({
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#0B1215',
+        white: '#FAF9F6',
+        primary: '#DD8F16',
+        secondary: '#FFD166',
+        light: '#FFEDC5',
+        dark: '#263B2B',
+        accent: '#0590E5',
+      },
+    },
   },
   plugins: [],
 });
