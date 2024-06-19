23f50854fb04302bff25124bd0495402a74382ef
Medou13500 <medoubouzid0@gmail.com>
2024-06-19 11:02:32 +0200

    feat :Creation de formulaire de contact

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./pages/Home.jsx";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import "./App.css";
+import Formulaire from './components/Formulaire/Formulaire.jsx'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <main className="flex-grow">
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/formulaire" element={<Formulaire/>} />
             </Routes>
           </main>
           {/* <Footer /> */}
