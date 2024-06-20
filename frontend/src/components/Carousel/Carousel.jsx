c9c6a1defc9cb06440aebe96f27614074d198bc3
Guillaume Martinez <guillaume.martinez.1804@gmail.com>
2024-06-20 10:03:16 +0200

    Ajout du composant article

--- frontend/src/components/Carousel/Carousel.jsx
+++ frontend/src/components/Carousel/Carousel.jsx
@@ -65,13 +65,13 @@ const CarouselDefault = () => {
       </div>
       <button
         onClick={() => prevSlide()}
-        className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full"
+        className="absolute top-1/2 left-6 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full"
       >
         &lt;
       </button>
       <button
         onClick={() => nextSlide()}
-        className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full"
+        className="absolute top-1/2 right-6 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full"
       >
         &gt;
       </button>
