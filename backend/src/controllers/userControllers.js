fddeb773983bf4ee9b053f35c9f3cb2136009a74
Emmanuelle-S <emmanuelle-sanchez@hotmail.fr>
2024-06-26 11:34:19 +0200

    the hashed password is no longer deleted when the profile is updated

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -30,13 +30,25 @@ const read = (req, res) => {
       res.sendStatus(500);
     });
 };
+// rajoute logique pour pas supprimer le mdp
 
-const edit = (req, res) => {
+const edit = async (req, res) => {
   const user = req.body;
 
   // TODO validations (length, format...)
 
   user.id = parseInt(req.params.id, 10);
+
+  if (user.user_pass) {
+    try {
+      user.user_pass = await argon2.hash(user.user_pass);
+    } catch (err) {
+      console.error(err);
+      return res.sendStatus(500);
+    }
+  } else {
+    delete user.user_pass;
+  }
 
   models.user
     .update(user)
