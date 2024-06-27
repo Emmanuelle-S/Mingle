6d45094041dd58f934397e4cbb10b79f5d54b1e1
Emmanuelle-S <emmanuelle-sanchez@hotmail.fr>
2024-06-27 13:04:35 +0200

    add comment

--- frontend/src/components/Profil/EditProfil.jsx
+++ frontend/src/components/Profil/EditProfil.jsx
@@ -4,7 +4,6 @@ import * as Yup from 'yup';
 import { useNavigate, useLocation } from 'react-router-dom';
 import axios from "axios";
 
-// Validation schema using Yup
 const validationSchema = Yup.object({
   username: Yup.string().required('Champs obligatoire'),
   mail: Yup.string().email('Adresse mail invalide').required('Champs obligatoire'),
@@ -13,9 +12,10 @@ const validationSchema = Yup.object({
   service_type: Yup.string().required('Champs obligatoire'),
   avatar: Yup.mixed(),
 });
+//  Définition du schéma de validation pour le formulaire utilisant Yup.
 
-// Custom input components
 const MyTextInput = ({ label, ...props }) => {
+  // Déclaration du composant fonctionnel MyTextInput qui prend label et d'autres props en paramètres
   const [field, meta] = useField(props);
   return (
     <div className="mb-4">
