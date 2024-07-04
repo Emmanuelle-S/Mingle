6396476687445bc2ce024a4b57ea36afcd746bac
Medou13500 <medoubouzid0@gmail.com>
2024-07-04 14:27:41 +0200

    fix: le bouton contacter nous sur les liste des categorie

--- frontend/src/components/Listedeservice/CardList.jsx
+++ frontend/src/components/Listedeservice/CardList.jsx
@@ -1,19 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
 const Card = ({ card }) => {
   return (
-    <div className="bg-white shadow-md rounded-lg p-4 m-2 w-64 h-auto">
-      <h3 className="text-lg font-bold mb-2">{card.titre_catégorie}</h3>
-      {card.category_image ? (
-        <img src={card.category_image} alt={card.titre_catégorie} className="w-full h-32 object-cover rounded-lg" />
-      ) : (
-        <p className="text-gray-600">Aucune image</p>
-      )}
-     
+    <div className="bg-white shadow-md rounded-lg p-4 m-2 w-80 h-80 flex flex-col justify-between border border-black">
+      <div>
+        <h3 className="text-lg font-bold mb-2">{card.titre_catégorie}</h3>
+        {card.category_image ? (
+          <img src={card.category_image} alt={card.titre_catégorie} className="w-full h-32 object-cover rounded-lg" />
+        ) : (
+          <p className="text-gray-600">Aucune image</p>
+        )}
+      </div>
+      <div className='flex justify-end'>
+        <button className="bg-accent text-white p-2  rounded-md">
+          Contacter-nous
+        </button>
+      </div>
     </div>
   );
 };
+
+
+
 
 const CardList = () => {
   const [cards, setCards] = useState([]);
