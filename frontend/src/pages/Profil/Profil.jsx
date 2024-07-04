7ac26d8db1d8fbf05430a151ec4654cd2448cc1a
Emmanuelle <emmanuelle-sanchez@hotmail.fr>
2024-07-04 19:53:11 +0200

    ability to consult a profile when not logged in

--- frontend/src/pages/Profil/Profil.jsx
+++ frontend/src/pages/Profil/Profil.jsx
@@ -1,9 +1,5 @@
-// npm install react-toastify
-
-
 import React, { useState, useEffect } from "react";
-import axios from "axios"; 
-// effectuer des requêtes HTTP.
+import axios from "axios"; // effectuer des requêtes HTTP.
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PersonalInfo from "@components/Profil/PersonalInfo";
@@ -14,50 +10,40 @@ import "./Profil.css";
 const Profil = () => {
   // Déclaration du composant fonctionnel Profil
   const [userData, setUserData] = useState(null); //Rappel useState : permet de gérer les états
-  // Utilisation du hook useState pour créer une variable d'état userData initialisée à null, et une fonction setUserData pour la mettre à jour. => userData = stocker les données utilisateur, initialisé à null.
-  const [isDeleted, setIsDeleted] = useState(false); 
-  // indiquer si le profil a été supprimé, initialisé à false.
-  const isLoggedIn = true;
-  // Déclaration d'une constante isLoggedIn avec une valeur true (indique que l'utilisateur est connecté).
+  const [isDeleted, setIsDeleted] = useState(false); // indiquer si le profil a été supprimé, initialisé à false.
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Etat pour savoir si l'utilisateur est connecté
   const navigate = useNavigate();
 
-  const { userId: routeUserId } = useParams();
-  // récupère des paramètres de l'URL.
-  const currentUserId = localStorage.getItem("userId");
-  // récupère des paramètres du local storage
-  const userId = routeUserId || currentUserId;
-  // compare les données récupérées du local storage et de l'URL
-  const isCurrentUser = userId === currentUserId;
-  // vérifie si l'utilisateur actuel est celui du profil
+  const { userId: routeUserId } = useParams(); // récupère des paramètres de l'URL.
+  const currentUserId = localStorage.getItem("userId"); // récupère des paramètres du local storage
+  const userId = routeUserId || currentUserId; // compare les données récupérées du local storage et de l'URL
+  const isCurrentUser = userId === currentUserId; // vérifie si l'utilisateur actuel est celui du profil
 
   const fetchData = async () => {
     // Déclaration d'une fonction asynchrone fetchData pour récupérer les données utilisateur.
     try {
-      const token = localStorage.getItem("token");
-      // Récupération du token d'authentification depuis le localStorage.
+      const token = localStorage.getItem("token"); // Récupération du token d'authentification depuis le localStorage.
+      const headers = token ? { Authorization: `Bearer ${token}` } : {};
 
       const userResponse = await axios.get(
         `http://localhost:5000/users/${userId}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
+        { headers }
       );
-      //  Envoi d'une requête GET pour récupérer les données de l'utilisateur en utilisant axios, avec le token dans les en-têtes de la requête
-      setUserData(userResponse.data);
-      // Mise à jour de l'état userData avec les données reçues de la réponse.
+      //  Envoi d'une requête GET pour récupérer les données de l'utilisateur en utilisant axios, avec le token dans les en-têtes de la requête si disponible
+      setUserData(userResponse.data); // Mise à jour de l'état userData avec les données reçues de la réponse.
+
+      if (token) {
+        setIsLoggedIn(true); // Si un token est présent, l'utilisateur est connecté
+      }
     } catch (error) {
-      console.error("Error fetching data", error);
-      // Gestion des erreurs en cas d'échec de la requête, avec un log de l'erreur.
+      console.error("Error fetching data", error); // Gestion des erreurs en cas d'échec de la requête, avec un log de l'erreur.
     }
   };
 
   const handleDeleteProfile = async () => {
     //  Déclaration d'une fonction asynchrone handleDeleteProfile pour supprimer le profil utilisateur.
     try {
-      const token = localStorage.getItem("token");
-      // Récupération du token d'authentification depuis le localStorage.
+      const token = localStorage.getItem("token"); // Récupération du token d'authentification depuis le localStorage.
 
       const response = await axios.delete(
         `http://localhost:5000/users/${userId}`,
@@ -70,30 +56,22 @@ const Profil = () => {
       // Envoi d'une requête DELETE pour supprimer le profil de l'utilisateur en utilisant axios, avec le token dans les en-têtes de la requête.
 
       if (response.status === 204) {
-        console.log("Profil supprimé avec succès.");
-        //  Vérification si la réponse de la requête indique une suppression réussie (statut 204) + log d'un message de succès
-        localStorage.removeItem("userId");
-        // Suppression de l'ID de l'utilisateur du localStorage.
-        localStorage.removeItem("token");
-        // Suppression du token du localStorage.
+        console.log("Profil supprimé avec succès."); //  Vérification si la réponse de la requête indique une suppression réussie (statut 204) + log d'un message de succès
+        localStorage.removeItem("userId"); // Suppression de l'ID de l'utilisateur du localStorage.
+        localStorage.removeItem("token"); // Suppression du token du localStorage.
         toast.success("Votre profil a été supprimé avec succès.", {
           // Utilisation de react-toastify et afficher une popup en cas de succès
-          position: "top-center", 
-          // Position centrée en haut
-          className: "custom-toast", 
-          // Classe CSS personnalisée
-          autoClose: 2000, 
-          // Durée de fermeture automatique
+          position: "top-center", // Position centrée en haut
+          className: "custom-toast", // Classe CSS personnalisée
+          autoClose: 2000, // Durée de fermeture automatique
         });
-       
+
         setIsDeleted(true);
       } else {
-        console.error("Erreur lors de la suppression du profil");
-        // Log d'une erreur si la suppression n'est pas réussie.
+        console.error("Erreur lors de la suppression du profil"); // Log d'une erreur si la suppression n'est pas réussie.
       }
     } catch (error) {
-      console.error("Erreur lors de la suppression du profil", error);
-      // Gestion des erreurs en cas d'échec de la requête, avec un log de l'erreur.
+      console.error("Erreur lors de la suppression du profil", error); // Gestion des erreurs en cas d'échec de la requête, avec un log de l'erreur.
     }
   };
 
@@ -122,7 +100,6 @@ const Profil = () => {
   };
   // naviguer vers le tableau de bord si l'utilisateur est connecté.
 
-
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-6 text-center text-darkslategray">
@@ -134,21 +111,25 @@ const Profil = () => {
         isCurrentUser={isCurrentUser}
         onDelete={handleDeleteProfile}
       />
-      {/* Rendu du composant PersonalInfo avec les props userData, isLoggedIn,isCurrentUser et onDelete. */}
+      {/* Rendu du composant PersonalInfo avec les props userData, isLoggedIn, isCurrentUser et onDelete. */}
       <div className="mt-8 bg-white rounded-lg border border-gray-300 shadow-2xl p-4">
         <h2 className="text-2xl font-extrabold mb-6 text-center text-darkslategray">
           Services publiés
         </h2>
         {/* <ServicesCarousel services={userData.services} isLoggedIn={isLoggedIn} /> */}
         <div className="text-center mt-4">
-          <button
-            className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded-lg text-lg font-medium focus:outline-none focus:shadow-outline"
-            onClick={handleSeeMoreClick}
-          >
-            Voir plus
-          </button>
+          {isLoggedIn ? (
+            <button
+              className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded-lg text-lg font-medium focus:outline-none focus:shadow-outline"
+              onClick={handleSeeMoreClick}
+            >
+              Voir plus
+            </button>
+          ) : (
+            <p className="text-gray-500">Connectez-vous pour voir plus de détails</p>
+          )}
         </div>
-        </div>
+      </div>
       <ToastContainer />
       {/* Ajout du ToastContainer pour afficher les notifications */}
     </div>
