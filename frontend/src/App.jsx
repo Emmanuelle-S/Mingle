773f1d1ebaf05cb45adb648a53b2ffbbdf47c41a
Emmanuelle <emmanuelle-sanchez@hotmail.fr>
2024-07-04 16:15:22 +0200

    create footer responsive,  miss some link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider } from "./contexts/AuthContext";
 import { ServiceProvider } from "./contexts/ServiceContext";
 import "./App.css";
-import axios from 'axios';
+import axios from "axios";
 import Header from "./components/Header/Header.jsx";
 import ChatBubble from "./pages/Message/Bubble";
-import AnimatedRoutes from './components/AnimatedRoutes/AnimatedRoutes'; // Déplacez AnimatedRoutes dans un fichier séparé
-import { jwtDecode } from 'jwt-decode';
-import { parseJSON } from 'date-fns';
+import AnimatedRoutes from "./components/AnimatedRoutes/AnimatedRoutes"; // Déplacez AnimatedRoutes dans un fichier séparé
+import { jwtDecode } from "jwt-decode";
+// import { parseJSON } from "date-fns";
+import Footer from "@components/Footer/Footer";
 
 function App() {
   const [currentToken, setCurrentToken] = useState(null);
 
   const [users, setUsers] = useState([]);
   const [friends, setFriends] = useState([]);
-  const [conversations, setConversations] = useState([]); 
+  const [conversations, setConversations] = useState([]);
 
   const [user, setUser] = useState(null);
-  
+
   const [userFriends, setUserFriends] = useState(null);
-  
 
   const fetchMingle = async (userId) => {
     try {
       const responseUsers = await axios.get("http://localhost:5000/users");
-      setUsers(responseUsers.data)
+      setUsers(responseUsers.data);
 
       const responseFriends = await axios.get("http://localhost:5000/friends");
-      const filteredFriends = await responseFriends.data.filter(friend => friend.user_id === userId);
+      const filteredFriends = await responseFriends.data.filter(
+        (friend) => friend.user_id === userId
+      );
       setFriends(filteredFriends);
 
-      const responseConversation = await axios.get("http://localhost:5000/conversation");
-      const filteredConversation = await responseConversation.data.filter(conversation => (conversation.user_id === userId) || (conversation.friend_id === userId));
+      const responseConversation = await axios.get(
+        "http://localhost:5000/conversation"
+      );
+      const filteredConversation = await responseConversation.data.filter(
+        (conversation) =>
+          conversation.user_id === userId || conversation.friend_id === userId
+      );
       setConversations(filteredConversation);
-    } 
-    catch (error) {
+    } catch (error) {
       console.error("Error fetching data", error);
     }
   };
 
-
-  // Recupére l'id dans le payload via jwt-decode 
+  // Recupére l'id dans le payload via jwt-decode
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       try {
         const decodedToken = jwtDecode(token);
@@ -53,7 +58,7 @@ function App() {
           console.log("Token is missing 'sub' property");
         }
       } catch (error) {
-        console.error('Error decoding token:', error);
+        console.error("Error decoding token:", error);
       }
     } else {
       console.log("No token found in localStorage");
@@ -67,34 +72,38 @@ function App() {
     }
   }, [currentToken]);
 
-
   // Récupére l'user de la session
   useEffect(() => {
     if (users) {
-      const filteredUser = users.filter(user => user.id === currentToken);
+      const filteredUser = users.filter((user) => user.id === currentToken);
       setUser(filteredUser[0]);
     }
-  }, [users])
+  }, [users]);
 
   // Récupére les data des amis du user et définis les conversations liés à ceux-ci
   useEffect(() => {
     if (friends) {
-      const parseFriends = friends[0]?.friends ? JSON.parse(friends[0]?.friends) : [];
-      const filteredUserFriends = parseFriends.map(friendId => 
-        users.find(user => user.id === friendId)
-      ).filter(user => user !== undefined);      // Ajout de la logique de filtre des conversations ici
-      const filteredConversation = friends.map(friend => {
-        const conversation = conversations.filter(conversation => conversation.friend_id === friend.friends);
-        return {conversation};
+      const parseFriends = friends[0]?.friends
+        ? JSON.parse(friends[0]?.friends)
+        : [];
+      const filteredUserFriends = parseFriends
+        .map((friendId) => users.find((user) => user.id === friendId))
+        .filter((user) => user !== undefined); // Ajout de la logique de filtre des conversations ici
+      const filteredConversation = friends.map((friend) => {
+        const conversation = conversations.filter(
+          (conversation) => conversation.friend_id === friend.friends
+        );
+        return { conversation };
       }); // Ajout de la logique de filtre de conversation ici
       setUserFriends(filteredUserFriends);
       setConversations(filteredConversation);
     }
-  }, [friends])
-
+  }, [friends]);
 
   const fetchConversation = (conversationId) => {
-    const getConversationByConvId = conversations.filter(conv => conv.id === conversationId);
+    const getConversationByConvId = conversations.filter(
+      (conv) => conv.id === conversationId
+    );
     return getConversationByConvId;
   };
 
@@ -102,18 +111,18 @@ function App() {
     <AuthProvider>
       <ServiceProvider>
         <Router>
-      <div className='bgone flex flex-col min-h-screen' >
-          <Header />
-          <AnimatedRoutes />
-          <ChatBubble
-                user={user}
-                friends={userFriends}
-                conversations={conversations}
-                setConversations={setConversations}
-                fetchConversation={fetchConversation}
-              />
-          
-              </div>
+          <div className="bgone flex flex-col min-h-screen">
+            <Header />
+            <AnimatedRoutes />
+            <ChatBubble
+              user={user}
+              friends={userFriends}
+              conversations={conversations}
+              setConversations={setConversations}
+              fetchConversation={fetchConversation}
+            />
+            <Footer />
+          </div>
         </Router>
       </ServiceProvider>
     </AuthProvider>
---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark p-4 relative z-50">
+      <div className="container mx-auto flex flex-col items-center text-center">
+        <nav className="flex flex-col md:flex-row md:space-x-4 items-center">
+          <Link to="/FAQ" className="text-white hover:text-gray-300">FAQ</Link>
+          <Link to="/ConditionsUtilisation" className="text-white hover:text-gray-300">Conditions d'utilisation</Link>
+          <Link to="/PolitiqueDeConfidentialite" className="text-white hover:text-gray-300">Politique de confidentialité</Link>
+          <Link to="/formulaire" className="text-white hover:text-gray-300">Contact</Link>
+          <Link to="/about" className="text-white hover:text-gray-300">À propos</Link>
+        </nav>
+      </div>
+      <div className="container mx-auto text-center mt-4">
+        <p className="text-white text-sm">
+          &copy; 2024 Mingle. Tous droits réservés.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
