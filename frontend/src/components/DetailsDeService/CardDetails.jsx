c8f168f28c800ef1ee1e85e7a45f1124c3752e85
Medou13500 <medoubouzid0@gmail.com>
2024-07-08 11:09:17 +0200

    feat: commencement details service

--- frontend/src/components/DetailsDeService/CardDetails.jsx
+++ frontend/src/components/DetailsDeService/CardDetails.jsx
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function CardDetail() {
-  const [card, setCardDetail] = useState([]);
+  const [card, setCardDetail] = useState(null);
 
   const fetchDataDetail = async () => {
     try {
       const response = await axios.get("http://localhost:5000/categoryservice");
-      console.log('response', response.data); // Ajoutez cette ligne pour vérifier les données
-      setCardDetail(response.data);
+      console.log('response', response.data);
+      if (response.data.length > 0) {
+        setCardDetail(response.data[0]);
+      }
     } catch (error) {
       console.error('Erreur lors de la récupération des données', error.message || error);
     }
@@ -18,21 +20,21 @@ function CardDetail() {
     fetchDataDetail();
   }, []);
 
+  if (!card) {
+    return <div>Chargement...</div>;
+  }
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="bg-gray-500 shadow-md rounded-lg p-6 m-4 w-full h-96 max-w-2xl flex flex-col justify-between">
-        <h3 className="text-lg font-bold mb-4 text-center">{card.titre_catégorie}</h3>
+    <div className="flex justify-center items-center h-dvh">
+      <div className="bg-primary shadow-md rounded-lg p-6 m-4 w-full h-[600px] max-w-2xl flex flex-col justify-between">
+        <p className="text-white text-center mb-6">{card.category_description}</p>
         <div className="flex flex-wrap justify-center mb-6">
-          {card.category_image ? (
-            <img src={card.category_image} alt={card.titre_catégorie} className="w-full h-48 object-cover rounded-lg mb-4" />
-          ) : (
-            <p className="text-white">Aucune image</p>
+          {card.category_image && (
+            <img src={card.category_image} alt={card.titre_catégorie} className="w-full h-32 object-cover rounded-lg" />
           )}
         </div>
-        <p className="text-white text-center mb-6">{card.description}</p>
         <div className="mt-auto flex justify-end">
-          
+          <button className="bg-blue-500 text-center text-white p-2 rounded-md">Contacter Batman</button>
         </div>
       </div>
     </div>
